8045fbd29d8a32f404f20a069b2927edd6555ae5
javid-akthar <aktharjavid74@gmail.com>
2022-12-20 10:42:59 +0530

    up

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,39 +1,39 @@
 import React, { useState } from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 // importing the css
 import classes from "./Dropdown.module.css";
 // importing the bootstrap
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function Dropdown(props) {
-    // this is boolean indictor which maintains showing the options list on mouse
-    //  hover by user
+  // this is boolean indictor which maintains showing the options list on mouse
+  //  hover by user
   let [showList, setShowList] = useState(false);
-    //  if user selects a valid value this useState will maintain the valid value
+  //  if user selects a valid value this useState will maintain the valid value
   let [selectedValue, setSelectedValue] = useState("select");
-    // this is boolean indictor which maintains showing the selected data from
-    // dropdown after user selects a valid data
+  // this is boolean indictor which maintains showing the selected data from
+  // dropdown after user selects a valid data
   let [showSelectedValue, setShowSelectedValue] = useState(false);
 
-    // this is for implmenting the style  
+  // this is for implmenting the style
   let StyledListContainer = styled.li`
     &:hover {
-        background-color: rgb(201,229,238);
+      background-color: rgb(201, 229, 238);
     }
   `;
 
-    //   function showing the options list on mouse
-    //  hover by user
+  //   function showing the options list on mouse
+  //  hover by user
   function show(event) {
     setShowList(true);
   }
 
-//   if a user selects any value in option the option visibily need to be hidded 
-//   this function maintains that
+  //   if a user selects any value in option the option visibily need to be hidded
+  //   this function maintains that
   function hide(val) {
-    if(val === "select"){
-        setShowList(false);
-        return;
+    if (val === "select") {
+      setShowList(false);
+      return;
     }
     console.log(val);
     setSelectedValue(val);
@@ -43,33 +43,40 @@ function Dropdown(props) {
 
   let selectStyle = { width: "25%", margin: "auto", cursor: "pointer" };
 
-//   list of option values that user can select
-let optionList = props.optionList
+  //   list of option values that user can select
+  let optionList = props.optionList;
 
-//   creating the list of option with the input list
-  let element = optionList.map(function(item, index) {
-    return <StyledListContainer value={item} key={index} onClick={()=>{ hide(item)}} className="list-group-item" style={selectStyle}>
-     {item}
-  </StyledListContainer>
-})
+  //   creating the list of option with the input list
+  let element = optionList.map(function (item, index) {
+    return (
+      <StyledListContainer
+        value={item}
+        key={index}
+        onClick={() => {
+          hide(item);
+        }}
+        className="list-group-item"
+        style={selectStyle}
+      >
+        {item}
+      </StyledListContainer>
+    );
+  });
 
   return (
     <>
-    {showSelectedValue?
-        <h4 className={classes.selectElementData}>the selected value is "{selectedValue}"</h4>:null}
-    <div className={classes.selectContainer}>
-      <button className={classes.selectButton}onMouseOver={show}>
-        {selectedValue}
-      </button>
-      {showList ? (
-        <ul className="list-group">
-            {element}
-        </ul>
+      {showSelectedValue ? (
+        <h4 className={classes.selectElementData}>
+          the selected value is "{selectedValue}"
+        </h4>
       ) : null}
-      
-    </div>
-    
-    </>    
+      <div className={classes.selectContainer}>
+        <button className={classes.selectButton} onMouseOver={show}>
+          {selectedValue}
+        </button>
+        {showList ? <ul className="list-group">{element}</ul> : null}
+      </div>
+    </>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import Dropdown from './Dropdown'
+import React from "react";
+import Dropdown from "./Dropdown";
 
 function Home() {
-    //   list of option values that user can select
-    let optionList =[
-        "select",
-        "Yes",
-        "Probably Not"
-      ]
+  //   list of option values that user can select
+  let optionList = ["select", "Yes", "Probably Not"];
 
-    return (
-        <div>
-            <Dropdown optionList={optionList}/>   
-        </div>
-    );
+  return (
+    <div>
+      <Dropdown optionList={optionList} />
+    </div>
+  );
 }
 
 export default Home;
